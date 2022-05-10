f4c2baf98dbd01b28572fb5f58a577780ad30db1
huythai <dangerous235@gmail.com>
2022-05-10 16:53:07 +0700

    feat: sample test case

---
+++ scripts/stake1.ts
@@ -0,0 +1,81 @@
+import { ethers, network } from "hardhat";
+import { moveTime } from "../utils/move_time";
+require("dotenv").config(); 
+
+// const hre = require("hardhat");
+// const ethers = hre.ethers;
+// const network = hre.network;
+
+export async function stake() {
+    const VERSION = process.env.VERSION;
+
+    let stakingContractName = "StakingPool";
+    if (VERSION === "2") stakingContractName = "StakingPoolV2";
+
+    const SPOToken = await ethers.getContract("SPOToken");
+
+    const StakingPool = await ethers.getContract(stakingContractName);
+
+    const owners = await ethers.getSigners();
+
+    const owner = owners[0];
+    const alice = owners[1];
+
+    // console.log(owners);
+
+    // const accounts = await hre.ethers.getSigners()
+
+    // const balance = await SPOToken.balanceOf(StakingPool.address);
+    // console.log(balance.toString());
+
+    const sendToUserTx = await SPOToken.transfer(
+        alice.address,
+        "150000000000000000000000"
+    );
+
+    await SPOToken.connect(alice).approve(
+        StakingPool.address,
+        "150000000000000000000000"
+    );
+
+    const balance_alice = await SPOToken.balanceOf(alice.address);
+    console.log(`Alice's balance ${balance_alice.toString()}`);
+
+    const allowanceAlicePool = await SPOToken.allowance(
+        alice.address,
+        StakingPool.address
+    );
+    console.log(`allowance ${allowanceAlicePool.toString()}`);
+    await StakingPool.connect(alice).stake('100000000000000000000000');
+
+    await moveTime(14 * 3600 * 24);
+    await StakingPool.connect(alice).stake('50000000000000000000000');
+
+    const balance_alice_1 = await SPOToken.balanceOf(alice.address);
+    console.log(`Alice's balance ${balance_alice_1.toString()}`);
+
+    await moveTime(15 * 3600 * 24);
+    await StakingPool.connect(alice).unstake('70000000000000000000000');
+
+    const balance_alice_2 = await SPOToken.balanceOf(alice.address);
+    console.log(`Alice's balance after unstake 700K SPO ${balance_alice_2.toString()}`);
+
+    await moveTime(15 * 3600 * 24);
+    await StakingPool.connect(alice).unstake('80000000000000000000000');
+
+
+    const balance_alice_3 = await SPOToken.balanceOf(alice.address);
+    console.log(`Alice's balance after unstake 800K SPO ${balance_alice_3.toString()}`);
+}
+
+// async function queryBalance(SPOToken: Contract, ) {
+//     const balance_alice = await SPOToken.balanceOf(alice.address);
+//     console.log(`Alice's balance ${balance_alice.toString()}`);
+    
+// }
+stake()
+    .then(() => process.exit())
+    .catch((err) => {
+        console.log(err);
+        process.exit(1);
+    });
